a3343bf315bd4274e9143f29515ad149497a9d92
nickfff-dev <hunchographics@gmail.com>
2022-02-03 18:01:15 +0300

    unsused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+import { TwitterTimelineEmbed} from 'react-twitter-embed';
 
 export default function App() {
   const [state, setState] = useState({
